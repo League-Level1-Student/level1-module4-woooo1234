f6684eda7bdee91794caa33d37519a664613d6db
league <league@192.168.1.82>
2024-02-17 17:59:53 -0800

    almost finished chuckle clicker.

---
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -0,0 +1,28 @@
+package _01_chuckle_clicker;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+
+public class ChuckleClicker {
+public static void main(String[] args) {
+	
+}
+
+private static void makeButtons() {
+	// TODO Auto-generated method stub
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	frame.add(button);
+	frame.add(button);
+	frame.add(panel);
+	if () {
+		
+	}
+	
+}
+}	
+	

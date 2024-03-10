b2dfd858e454264becab82df26ed10ad57062f14
matthew <matthewseul>
2024-03-09 17:59:06 -0800

    was just fine tuning some older projects and working some newer extras.

--- src/_01_chuckle_clicker/ChuckleClicker.java
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -1,28 +1,51 @@
 package _01_chuckle_clicker;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
-public class ChuckleClicker {
+public class ChuckleClicker implements  ActionListener {
 public static void main(String[] args) {
+	new ChuckleClicker () .makeButtons();
+
+}
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JButton JokeButton = new JButton("Joke");
+JButton PunchButton = new JButton("Punchline");
+
+private void makeButtons() {
+	// TODO Auto-generated method stub
+
+	panel.add(JokeButton);
+	panel.add(PunchButton);
+	frame.add(panel);
+	frame.pack();
+	JokeButton.addActionListener(this);
+	PunchButton.addActionListener(this);
+	frame.setVisible(true);
+	}
+
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+	if(e.getSource() == JokeButton) {
+		JOptionPane.showMessageDialog(null, "Whyd the chicken cross the road");
+	}
+	else if (e.getSource() == PunchButton) {
+		JOptionPane.showMessageDialog(null, "To get to the other side");
+	}
+}
 	
 }
 
-private static void makeButtons() {
-	// TODO Auto-generated method stub
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	JButton button = new JButton();
-	frame.add(button);
-	frame.add(button);
-	frame.add(panel);
-	if () {
-		
-	}
-	
-}
-}	
 	
--- src/_99_extra/_01_nasty_surprise/Trick.java
+++ src/_99_extra/_01_nasty_surprise/Trick.java
@@ -1,19 +1,40 @@
 package _99_extra._01_nasty_surprise;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Trick {
+public class Trick implements ActionListener {
 public static void main(String[]args ) {
-	JFrame frame = new JFrame();
-    JButton button = new JButton();
-	JButton button2 = new JButton();
-	
-	
-		
-	
+	new Trick () .trick();
 	
 }
+	JFrame frame = new JFrame();
+	JButton trick = new JButton("trick");
+	JButton treat = new JButton("treat");
+	JPanel panel = new JPanel();
+
+
+private void trick() {
+	panel.add(trick);
+	panel.add(treat);
+	frame.add(panel);
+	frame.pack();
+	trick.addActionListener(this);
+	
+}
+
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if(e.getSource())
+}
+	
+	
 	
 
 }
